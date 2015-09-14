b192e28350ae822a150341bb0699284888f887d5
Meet Udeshi <mudeshi1209@gmail.com>
2015-09-15 00:49:55 +0530

    Working code

--- app/src/main/java/org/iitb/moodi/ui/activity/SplashActivity.java
+++ app/src/main/java/org/iitb/moodi/ui/activity/SplashActivity.java
@@ -2,6 +2,11 @@ package org.iitb.moodi.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.facebook.Session;
+import com.facebook.widget.LoginButton;
 
 import org.iitb.moodi.R;
 
@@ -11,6 +16,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Session session = Session.getActiveSession();
+
+        if(session!=null && session.isOpened()) {
+            findViewById(R.id.fb_login_button).setVisibility(View.GONE);
+
+        } else {
+            findViewById(R.id.fb_login_button).setVisibility(View.VISIBLE);
+        }
+
+        ((LoginButton)findViewById(R.id.fb_login_button)).setReadPermissions("email");
     }
 
 
