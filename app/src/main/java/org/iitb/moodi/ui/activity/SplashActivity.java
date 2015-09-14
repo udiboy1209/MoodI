98da60285924d8479b99c6e70da73f7a9cd986f8
Meet Udeshi <mudeshi1209@gmail.com>
2015-09-15 00:30:11 +0530

    In process

---
+++ app/src/main/java/org/iitb/moodi/ui/activity/SplashActivity.java
@@ -0,0 +1,17 @@
+package org.iitb.moodi.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.iitb.moodi.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+}
