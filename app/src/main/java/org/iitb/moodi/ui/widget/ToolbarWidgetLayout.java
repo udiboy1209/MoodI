32513abe26872d3e5bdcb63ddad8215e35d93d10
Meet Udeshi <mudeshi1209@gmail.com>
2015-11-01 14:19:22 +0530

    Add toolbar widget layout

---
+++ app/src/main/java/org/iitb/moodi/ui/widget/ToolbarWidgetLayout.java
@@ -0,0 +1,71 @@
+package org.iitb.moodi.ui.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.Toolbar;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import org.iitb.moodi.R;
+
+public class ToolbarWidgetLayout extends LinearLayout {
+    private Toolbar mToolbar;
+    private View mWidget;
+
+    public ToolbarWidgetLayout(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ToolbarWidgetLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ToolbarWidgetLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.ToolbarWidgetLayout, defStyle, 0);
+
+        setOrientation(LinearLayout.VERTICAL);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        for(int i=0; i<getChildCount();i++) {
+            if(getChildAt(i) instanceof Toolbar) {
+                mToolbar = (Toolbar) getChildAt(i);
+
+                if (i + 1 < getChildCount()) {
+                    mWidget = getChildAt(i + 1);
+                    i++;
+                }
+            } else {
+                removeViewAt(i);
+                i--;
+            }
+        }
+    }
+
+    public void setWidget(View v){
+        removeView(mWidget);
+        if(v!=null) addView(v);
+    }
+
+    public Toolbar getToolbar(){
+        return mToolbar;
+    }
+}
