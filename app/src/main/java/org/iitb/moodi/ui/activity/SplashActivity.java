d494cd5aba52b280fddcb945de6b8f460ce9e18f
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-11-24 03:06:43 +0530

    Adding new FB API, Facebook Login working

--- app/src/main/java/org/iitb/moodi/ui/activity/SplashActivity.java
+++ app/src/main/java/org/iitb/moodi/ui/activity/SplashActivity.java
@@ -6,52 +6,70 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
-import com.facebook.Request;
-import com.facebook.Response;
-import com.facebook.Session;
-import com.facebook.model.GraphUser;
-import com.facebook.widget.LoginButton;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 
 import org.iitb.moodi.R;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class SplashActivity extends AppCompatActivity {
 
     private static final String TAG = "SplashActivity";
-
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_splash);
-        Session session = Session.getActiveSession();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        loginButton.setReadPermissions(Arrays.asList("email"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+                                Log.d(TAG,response.toString());
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
 
-        if(session!=null && session.isOpened()) {
-            findViewById(R.id.fb_login_button).setVisibility(View.GONE);
-            startActivity(new Intent(this, MainActivity.class));
-        } else {
-            findViewById(R.id.fb_login_button).setVisibility(View.VISIBLE);
-        }
+            @Override
+            public void onCancel() {
+            }
 
-        ((LoginButton)findViewById(R.id.fb_login_button)).setReadPermissions("email");
+            @Override
+            public void onError(FacebookException exception) {
+                Log.e(TAG, exception.getMessage());
+            }
+        });
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.d("LoginDebug", "ActivityResult");
-        Session.getActiveSession().onActivityResult(this, requestCode,
-                resultCode, data);
-
-        getBiodata();
-
         super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    public void getBiodata(){
-        Log.d("LoginDebug", "getBioData");
-        Request.executeMeRequestAsync(Session.getActiveSession(), new Request.GraphUserCallback() {
-            @Override
-            public void onCompleted(GraphUser graphUser, Response response) {
-                Log.i(TAG, "fbid : " + graphUser.getId() + " " + graphUser.getName() + " " + (String) graphUser.getProperty("email") + (String) graphUser.getProperty("gender"));
-            }
-        });
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
