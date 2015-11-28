f584cf3344033c39861f524a739a6a7243dd0cc7
Meet Udeshi <mudeshi1209@gmail.com>
2015-11-28 10:20:48 +0530

    Schedule Activity made

---
+++ app/src/main/java/org/iitb/moodi/ui/activity/ScheduleActivity.java
@@ -0,0 +1,91 @@
+package org.iitb.moodi.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import org.iitb.moodi.R;
+import org.iitb.moodi.ui.fragment.NavigationDrawerFragment;
+
+
+public class ScheduleActivity extends BaseActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Intent i = new Intent(getBaseContext(), RegistrationActivity.class);
+        startActivity(i);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //collapsingToolbar = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mToolbar.setTitle("Schedule");
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void gotoTimeline(View v){
+        int id = Integer.valueOf((String)v.getTag());
+        Intent timeline = new Intent();
+        //timeline.setClass(ScheduleActivity.this, TimelineActivity.class);
+        timeline.putExtra("day",id);
+        startActivity(timeline);
+    }
+}
